6ffc4e8b82eb04a640f3517b54c7e2f3481ae484
Grace Cheung <cgrace0309@gmail.com>
2019-08-20 16:12:38 +0800

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,38 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:title], details: params[:details])
+    @task.save
+    @tasks = Task.all
+    render 'index'
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:title], details: params[:details])
+    @tasks = Task.all
+    render 'index'
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    @tasks = Task.all
+    render 'index'
+  end
 end
